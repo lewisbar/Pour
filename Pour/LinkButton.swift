1c4677cc78c1cb4df423985dbebc447a0713a02b
Lennart Wisbar <lewisbar.git@gmail.com>
2019-11-18 22:25:44 +0100

    Deleted unneeded template code

---
+++ Pour/LinkButton.swift
@@ -0,0 +1,21 @@
+//
+//  LinkButton.swift
+//  Pour
+//
+//  Created by Lennart Wisbar on 18.11.19.
+//  Copyright © 2019 Lennart Wisbar. All rights reserved.
+//
+
+import UIKit
+
+class LinkButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
