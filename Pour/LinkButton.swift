b81bbdfc467a0728995c975ab344f114515816c6
Lennart Wisbar <lewisbar.git@gmail.com>
2019-11-18 22:26:13 +0100

    Added LinkButton class

--- Pour/LinkButton.swift
+++ Pour/LinkButton.swift
@@ -10,12 +10,15 @@ import UIKit
 
 class LinkButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .green
+        self.titleLabel?.textColor = .white
+        self.titleLabel?.text = "Link to Evernote"
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
